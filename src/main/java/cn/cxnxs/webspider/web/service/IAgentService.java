929ba2997b202516756436ebff797fb782197360
mengjinyuan <782399171@qq.com>
2020-11-24 16:32:16 +0800

    service

--- src/main/java/cn/cxnxs/webspider/web/service/IAgentService.java
+++ src/main/java/cn/cxnxs/webspider/web/service/IAgentService.java
@@ -3,6 +3,8 @@ package cn.cxnxs.webspider.web.service;
 import cn.cxnxs.webspider.web.entity.Agent;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Map;
+
 /**
  * <p>
  * 爬虫任务配置信息 服务类
@@ -13,4 +15,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IAgentService extends IService<Agent> {
 
+    /**
+     * 获取agent配置信息
+     * @param agentType agent类型
+     * @return
+     */
+    Map<String,Object> getAgentConfig(String agentType);
 }
---
+++ src/main/java/cn/cxnxs/webspider/web/service/IAgentTypeService.java
@@ -0,0 +1,16 @@
+package cn.cxnxs.webspider.web.service;
+
+import cn.cxnxs.webspider.web.entity.AgentType;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * agent定义信息 服务类
+ * </p>
+ *
+ * @author mengjinyuan
+ * @since 2020-11-17
+ */
+public interface IAgentTypeService extends IService<AgentType> {
+
+}
