25811ffb345ae217767e53cfb9dac15e31e4aeb5
mengjinyuan <782399171@qq.com>
2020-11-26 09:57:22 +0800

    登录

--- src/main/java/cn/cxnxs/webspider/web/controller/IndexController.java
+++ src/main/java/cn/cxnxs/webspider/web/controller/IndexController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
     @GetMapping("/")
     public String index(){
         return "index";
