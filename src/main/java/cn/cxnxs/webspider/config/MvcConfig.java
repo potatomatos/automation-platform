6d22bdadc01a30f8bed01909f8968a1d6ec792f5
mengjinyuan <782399171@qq.com>
2020-11-26 10:00:11 +0800

    配置权限拦截器

--- src/main/java/cn/cxnxs/webspider/config/MvcConfig.java
+++ src/main/java/cn/cxnxs/webspider/config/MvcConfig.java
@@ -1,9 +1,12 @@
 package cn.cxnxs.webspider.config;
 
+import cn.cxnxs.webspider.web.interceptor.PermissionInterceptor;
 import cn.cxnxs.webspider.web.interceptor.ResponseResultInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
@@ -21,9 +24,20 @@ public class MvcConfig implements WebMvcConfigurer {
     @Resource
     private ResponseResultInterceptor responseResultInterceptor;
 
+    @Resource
+    private PermissionInterceptor permissionInterceptor;
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        log.info("------设置权限拦截器------");
+        /*registry.addInterceptor(permissionInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/static/**")
+                .excludePathPatterns("/captcha")
+                .excludePathPatterns("/login")
+                .excludePathPatterns("/user/login")
+        ;*/
 
         log.info("------设置统一接口拦截器------");
         registry.addInterceptor(responseResultInterceptor)
---
+++ src/main/java/cn/cxnxs/webspider/web/interceptor/PermissionInterceptor.java
@@ -0,0 +1,52 @@
+package cn.cxnxs.webspider.web.interceptor;
+
+import cn.cxnxs.webspider.web.vo.Result;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * <p>权限拦截器</p>
+ *
+ * @author mengjinyuan
+ * @date 2020-10-13 22:29
+ **/
+@Slf4j
+@Component
+public class PermissionInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        String token=request.getParameter("token");
+        Result<String> result = null;
+        boolean flag=true;
+        response.setContentType("json");
+        response.setCharacterEncoding("utf8");
+        if (token!=null){
+            String token1= (String) request.getSession().getAttribute("token");
+            if (!token.equals(token1)){
+                log.error("======token非法======");
+                result=Result.failure(Result.ResultEnum.ILLEGAL_TOKEN,Result.ResultEnum.ILLEGAL_TOKEN.getInfo(),null);
+                flag=false;
+            }
+        }else {
+            log.error("======token非法======");
+            result=Result.failure(Result.ResultEnum.ILLEGAL_TOKEN,Result.ResultEnum.ILLEGAL_TOKEN.getInfo(),null);
+            flag=false;
+        }
+        if (request.getHeader("x-requested-with") != null && request.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest")) {
+            //返回json
+            response.setStatus(403);
+            response.getWriter().write(JSON.toJSONString(result));
+        }else {
+            //返回页面
+            response.setStatus(403);
+        }
+        return flag;
+    }
+}
