f4c5428f98c7b2c9ee88846fcbfc99aba2ddfc90
mengjinyuan <782399171@qq.com>
2021-02-14 00:17:19 +0800

    添加下一个任务执行

--- src/main/java/cn/cxnxs/webspider/core/quartz/DelayedJob.java
+++ src/main/java/cn/cxnxs/webspider/core/quartz/DelayedJob.java
@@ -3,28 +3,31 @@ package cn.cxnxs.webspider.core.quartz;
 import cn.cxnxs.webspider.core.Event;
 import cn.cxnxs.webspider.core.IAgent;
 import cn.cxnxs.webspider.utils.ObjectUtil;
-import cn.cxnxs.webspider.utils.SpringContextUtil;
-import cn.cxnxs.webspider.web.entity.Agent;
-import cn.cxnxs.webspider.web.entity.AgentType;
+import cn.cxnxs.webspider.utils.StringUtil;
 import cn.cxnxs.webspider.web.entity.Events;
 import cn.cxnxs.webspider.web.service.IAgentService;
 import cn.cxnxs.webspider.web.service.IEventsService;
-import com.alibaba.fastjson.JSONObject;
+import cn.cxnxs.webspider.web.vo.AgentTypeVo;
+import cn.cxnxs.webspider.web.vo.AgentVo;
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.google.common.util.concurrent.*;
+import lombok.SneakyThrows;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 /**
  * <p>定时任务类</p>
@@ -43,6 +46,12 @@ public class DelayedJob extends QuartzJobBean {
     @Autowired
     private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
+    @Autowired
+    private JobGenerator jobGenerator;
+
+    @Autowired
+    private TaskScheduler taskScheduler;
+
     private static final Logger logger = LoggerFactory.getLogger(DelayedJob.class);
 
     /**
@@ -51,44 +60,48 @@ public class DelayedJob extends QuartzJobBean {
     private Integer id;
 
     @Override
-    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    protected void executeInternal(JobExecutionContext jobExecutionContext) {
         try {
             logger.info("------定时任务开始执行------");
-            Agent agent = agentService.getById(id);
-            List<Events> events;
+            AgentVo agentVo = agentService.getAgentById(id);
+            AgentTypeVo agentType = agentVo.getAgentType();
             int pageNo = 1;
             int pageSize = 1000;
-            IAgent agentInstance = buildAgent(agent);
-            IPage<Events> eventsPage = eventsService.page(new Page<>(pageNo, pageSize),
-                    new QueryWrapper<Events>().eq("agent_id", id));
-            events = eventsPage.getRecords();
-            Future<List<Map<String, String>>> future = null;
-            if (events.size() == 0) {
-                TaskRunnable taskRunnable = new TaskRunnable();
-                taskRunnable.setAgent(agentInstance);
-                future = threadPoolTaskExecutor.submit(taskRunnable);
+
+            //获取来源代理
+            List<AgentVo> sourceAgents = agentVo.getSourceAgents();
+            if (!agentType.getCanReceiveEvents()
+                    && sourceAgents.size() == 0) {
+                this.runTask(agentVo, null);
             } else {
-                while (events.size() > 0) {
-                    //将事件添加到代理
-                    for (Events event : events) {
-                        Event ev = new Event();
-                        ObjectUtil.transValues(event, Event.class);
-                        TaskRunnable taskRunnable = new TaskRunnable();
-                        taskRunnable.setAgent(agentInstance);
-                        taskRunnable.setEvent(ev);
-                        future = threadPoolTaskExecutor.submit(taskRunnable);
-                    }
-                    pageNo++;
-                    eventsPage = eventsService.page(new Page<>(pageNo, pageSize),
-                            new QueryWrapper<Events>().eq("agent_id", id));
+                List<Events> events;
+                if (sourceAgents.size() > 0) {
+                    List<Integer> sourceAgentsIdList = sourceAgents.stream().map(AgentVo::getId).collect(Collectors.toList());
+                    IPage<Events> eventsPage = eventsService.page(new Page<>(pageNo, pageSize),
+                            new QueryWrapper<Events>().in("agent_id", sourceAgentsIdList).isNull("locked_by"));
                     events = eventsPage.getRecords();
+                    while (events.size() > 0) {
+                        //将事件添加到代理
+                        for (Events event : events) {
+                            event = event.selectById();
+                            //判断该数据是否被其他线程处理
+                            if (StringUtil.isEmpty(event.getLockedBy())) {
+                                this.runTask(agentVo, event);
+                            } else {
+                                logger.info("该数据正在被线程");
+                            }
+                        }
+                        pageNo++;
+                        eventsPage = eventsService.page(new Page<>(pageNo, pageSize),
+                                new QueryWrapper<Events>().eq("agent_id", id));
+                        events = eventsPage.getRecords();
+                    }
+                } else {
+                    logger.info("数据来源未配置，跳过执行");
                 }
             }
-            logger.info("执行结果：{}", future.get());
         } catch (ClassNotFoundException e) {
             logger.error("定时任务执行失败,找不到指定的执行类", e);
-        } catch (InterruptedException | ExecutionException e) {
-            logger.error("线程运行发生异常", e);
         }
     }
 
@@ -100,12 +113,77 @@ public class DelayedJob extends QuartzJobBean {
         this.id = id;
     }
 
-    public IAgent buildAgent(Agent agent) throws ClassNotFoundException {
-        Integer typeId = agent.getType();
-        AgentType agentType = new AgentType().selectById(typeId);
-        Class<IAgent> agentClass = (Class<IAgent>) Class.forName(agentType.getHandler());
-        IAgent agentInstance = SpringContextUtil.getBean(agentClass);
-        agentInstance.option(JSONObject.parseObject(agent.getOptions()));
-        return agentInstance.option(JSONObject.parseObject(agent.getOptions()));
+    /**
+     * 执行任务
+     */
+    public void runTask(AgentVo agentVo, final Events ev) throws ClassNotFoundException {
+        Thread t = Thread.currentThread();
+        Event event = null;
+        if (ev != null) {
+            event = new Event();
+            ObjectUtil.transValues(ev, Event.class);
+            ev.setLockedBy(t.getName());
+            ev.updateById();
+        }
+        AgentTypeVo agentType = agentVo.getAgentType();
+        IAgent agentInstance = jobGenerator.buildAgent(agentVo);
+        TaskRunnable taskRunnable = new TaskRunnable();
+        taskRunnable.setAgent(agentInstance);
+        taskRunnable.setEvent(event);
+        ListeningExecutorService service = MoreExecutors.listeningDecorator(threadPoolTaskExecutor.getThreadPoolExecutor());
+        ListenableFuture<List<Map<String, String>>> future = service.submit(taskRunnable);
+        Futures.addCallback(future, new FutureCallback<List<Map<String, String>>>() {
+            @SneakyThrows
+            @Override
+            public void onSuccess(List<Map<String, String>> maps) {
+                logger.info("执行结果：{}", maps);
+                List<Events> eventsList = new ArrayList<>();
+                maps.forEach(map -> {
+                    Events eventAdd = new Events();
+                    eventAdd.setAgentId(id);
+                    eventAdd.setPayload(JSON.toJSONString(map));
+                    eventAdd.setCreatedAt(LocalDateTime.now());
+                    //是否保存事件
+                    if (agentType.getCanCreateEvents()) {
+                        eventAdd.insert();
+                    }
+                    eventsList.add(eventAdd);
+                });
+                runNextDelayedJobs(agentVo,eventsList);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                logger.error("线程运行发生异常,任务执行失败", throwable);
+            }
+        }, threadPoolTaskExecutor);
+    }
+
+    /**
+     * 执行下一个任务
+     *
+     * @param agentVo
+     */
+    public void runNextDelayedJobs(AgentVo agentVo,List<Events> events) throws SchedulerException, ClassNotFoundException {
+        logger.info("------执行下个代理------");
+        //查出所有下一级代理信息
+        List<AgentVo> receivers = agentVo.getReceiverAgents();
+        for (AgentVo receiver : receivers) {
+            //判断是否立即传播事件
+            AgentVo receiverAgentVo = agentService.getAgentById(receiver.getId());
+            AgentTypeVo agentType = receiverAgentVo.getAgentType();
+            //判断是不是定时任务，是：启动定时任务，否：直接执行任务
+            if (agentType.getCanBeScheduled()) {
+                //手动触发定时任务
+                TaskDetail taskDetail = new TaskDetail();
+                taskDetail.setJobName(receiverAgentVo.getName());
+                taskDetail.setTriggerName(agentType.getAgentTypeName());
+                taskScheduler.triggerJob(taskDetail);
+            } else {
+                for (Events event : events) {
+                    this.runTask(receiver, event);
+                }
+            }
+        }
     }
 }
